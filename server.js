d3e43ddab6430e4aa3d8f378d2e8fb4f8eb17497
franciscoalvarez <franchelaa1@gmail.com>
2026-02-26 21:25:43 +0000

    Disable API caching headers in Express

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ const testRoutes = require("./src/routes/test");
 
 const app = express();
 
+app.disable("etag");
+
+app.use("/api", (req, res, next) => {
+  res.set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
+  res.set("Pragma", "no-cache");
+  res.set("Expires", "0");
+  res.set("Surrogate-Control", "no-store");
+  next();
+});
+
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
 

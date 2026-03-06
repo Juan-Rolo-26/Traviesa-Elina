148d25658e1f35aa8d192de08a95ab269a22f2a0
franciscoalvarez <franchelaa1@gmail.com>
2026-03-06 16:51:34 +0000

    09

--- prisma/migrations/20260210025204_add_product_images/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "ProductImage" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "url" TEXT NOT NULL,
-    "productId" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "ProductImage_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
